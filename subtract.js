6f5a5459312d511f28e9c43406100f1ad25cc7dc
ssotirop <sotiris.sotiropoulos@gmail.com>
2020-01-05 11:15:17 +0000

    Revert "extra feature in subtract function"
    This reverts commit d696fd0b198bcca6c8f7ad051fa220ea02e8f280.

--- subtract.js
+++ subtract.js
@@ -2,10 +2,5 @@
 
 // Return subtraction of two numbers
 function subtract(a, b) {
-    if (bMINUSa === true) {
-	return b - a;
-    }
-    else {
-        return a - b;
-    }
+    return a - b;
 };
